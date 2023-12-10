d59cea9f210bce4873b6385498e0151d78dbf699
Tapo4ek-exe <am14102001@gmail.com>
2023-12-11 00:09:55 +0300

    Добавлены классы для инъекции зависимостей

---
+++ EShop.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EShop.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
---
+++ EShop.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using EShop.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EShop.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Db");
+            services.AddDbContext<EShopDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddScoped<IEShopDbContext>(provider =>
+                provider.GetService<EShopDbContext>());
+
+            return services;
+        }
+    }
+}
