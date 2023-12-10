1af518acf321cec4e249eaddb196d4c0e0396a6f
Tapo4ek-exe <am14102001@gmail.com>
2023-12-10 23:55:33 +0300

    Добавлена конфигурация Automapper

---
+++ EShop.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace EShop.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
