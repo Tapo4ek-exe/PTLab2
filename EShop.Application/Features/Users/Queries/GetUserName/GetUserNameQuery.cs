52951c116174f0b924906d2a0bef8d70ff64a5be
Tapo4ek-exe <am14102001@gmail.com>
2023-12-12 01:38:55 +0300

    Добавлен запрос для получения имени пользователя

---
+++ EShop.Application/Features/Users/Queries/GetUserName/GetUserNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EShop.Application.Features.Users.Queries.GetName
+{
+    public class GetUserNameQuery : IRequest<string>
+    {
+        public Guid UserId { get; set; }
+    }
+}
---
+++ EShop.Application/Features/Users/Queries/GetUserName/GetUserNameQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using EShop.Application.Common.Exceptions;
+using EShop.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Application.Features.Users.Queries.GetName
+{
+    public class GetUserNameQueryHandler : IRequestHandler<GetUserNameQuery, string>
+    {
+        private readonly IEShopDbContext _dbContext;
+
+        public GetUserNameQueryHandler(IEShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<string> Handle(GetUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(user => user.Id == request.UserId, cancellationToken);
+            if (user == null)
+            {
+                throw new NotFoundException(nameof(user), request.UserId);
+            }
+            return user.Name;
+        }
+    }
+}
